b442b8e30706a42291ff9de1d3511270736e271c
L672652 <672652@stud.hvl.no>
2022-10-11 11:31:02 +0200

    Fikset litt på starten

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -3,64 +3,74 @@ package no.hvl.dat100ptc.oppgave1;
 import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
+	private int time;
+	private double latitude;
+	private double longitude;
+	private double elevation;
 
 	// TODO - objektvariable
 		
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
+		
+		this.latitude=latitude;
+		this.time=time;
+		this.longitude=longitude;
+		this.elevation=elevation;
 
 		// TODO - konstruktør
 
 		// Fjern linjene med throw new etterhvert som metodene implementeres
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
 
 	}
 
 	// TODO - get/set metoder
 	public int getTime() {
+		return time;
 		
-		throw new UnsupportedOperationException(TODO.method());
+		
 		
 	}
 
 	public void setTime(int time) {
+		this.time=time;
 				
-		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public double getLatitude() {
+		return latitude;
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setLatitude(double latitude) {
+		this.latitude=latitude;
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getLongitude() {
+		return longitude;
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setLongitude(double longitude) {
+		this.longitude=longitude;
 		
 		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getElevation() {
+		return elevation;
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setElevation(double elevation) {
+		this.elevation=elevation;
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 	
