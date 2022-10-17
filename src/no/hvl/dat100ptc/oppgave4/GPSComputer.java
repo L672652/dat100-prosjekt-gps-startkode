1b90b55b52356f5f8839b9db1ec6cb9b31f34263
h180786 <potetjuice@gmail.com>
2022-10-17 15:10:10 +0200

    Ferdig med 4 f

--- src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
+++ src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
@@ -132,18 +132,29 @@ public class GPSComputer {
 	// beregn kcal gitt weight og tid der kjøres med en gitt hastighet
 	public double kcal(double weight, int secs, double speed) {
 
-		double kcal;
+		double kcal = weight * secs/speed;
 
 		// MET: Metabolic equivalent of task angir (kcal x kg-1 x h-1)
-		double met = 0;		
+		double met = 0;
 		double speedmph = speed * MS;
 
-		// TODO - START
+		if (speedmph<10) {
+			met = 4.0;
+		} else if (speedmph<=10||speedmph>=12) {
+			met = 6.0;
+		} else if (speedmph<12||speedmph>=14) {
+			met = 8.0;
+		} else if (speedmph<14||speedmph>=16) {
+			met = 10.0;
+		}  else if (speedmph<16||speedmph>=20) {
+			met = 12.0;
+		}else if (speedmph>20) {
+			met = 16.0;
+		}
 		
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
+		kcal = (met * weight * (secs/3600));
 		
+		return kcal;
 	}
 
 	public double totalKcal(double weight) {
