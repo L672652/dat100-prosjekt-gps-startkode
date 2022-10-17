85a7486aba7a677d9ab8da1f7104178f3b2c52ef
h180786 <potetjuice@gmail.com>
2022-10-17 23:24:39 +0200

    Oppdatert Oppgave 4

--- src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
+++ src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
@@ -8,9 +8,9 @@ import no.hvl.dat100ptc.oppgave2.GPSDataFileReader;
 import no.hvl.dat100ptc.oppgave3.GPSUtils;
 
 public class GPSComputer {
-	
+
 	private GPSPoint[] gpspoints;
-	
+
 	public GPSComputer(String filename) {
 
 		GPSData gpsdata = GPSDataFileReader.readGPSFile(filename);
@@ -21,109 +21,75 @@ public class GPSComputer {
 	public GPSComputer(GPSPoint[] gpspoints) {
 		this.gpspoints = gpspoints;
 	}
-	
+
 	public GPSPoint[] getGPSPoints() {
 		return this.gpspoints;
 	}
-	
+
 	// beregn total distances (i meter)
 	public double totalDistance() {
 
 		double distance = 0;
-		for(int i=0;i<gpspoints.length-1;i++) {
-			distance+= GPSUtils.distance(gpspoints[i], gpspoints[i+1]);
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+			distance += GPSUtils.distance(gpspoints[i], gpspoints[i + 1]);
 		}
 		return distance;
-		
-
-		// TODO - START
-
-
-		// TODO - SLUTT
-
 	}
 
 	// beregn totale høydemeter (i meter)
 	public double totalElevation() {
 
 		double elevation = 0;
-		for(int i=0;i<gpspoints.length-1;i++) {
-			
-			if(gpspoints[i].getElevation()<gpspoints[i+1].getElevation()) {
-				elevation+=gpspoints[i+1].getElevation()-gpspoints[i].getElevation();
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+
+			if (gpspoints[i].getElevation() < gpspoints[i + 1].getElevation()) {
+				elevation += gpspoints[i + 1].getElevation() - gpspoints[i].getElevation();
 			}
 		}
 		return elevation;
-		
-		//for(i=0;i<)
-
-		// TODO - START
-
-		// TODO - SLUTT
-
 	}
 
 	// beregn total tiden for hele turen (i sekunder)
 	public int totalTime() {
-		int time = gpspoints[gpspoints.length-1].getTime()-gpspoints[0].getTime();
-		
+		int time = gpspoints[gpspoints.length - 1].getTime() - gpspoints[0].getTime();
 
 		return time;
 
-
 	}
-		
+
 	// beregn gjennomsnitshastighets mellom hver av gps punktene
 
 	public double[] speeds() {
-		double[] farter=new double[gpspoints.length-1];
-		for(int i=0;i<gpspoints.length-1;i++) {
-			farter[i]=GPSUtils.speed(gpspoints[i], gpspoints[i+1]);
-			
+		double[] farter = new double[gpspoints.length - 1];
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+			farter[i] = GPSUtils.speed(gpspoints[i], gpspoints[i + 1]);
+
 		}
 		return farter;
-		
-		// TODO - START		// OPPGAVE - START
-		
-
-		// TODO - SLUTT
 
 	}
-	
+
 	public double maxSpeed() {
-		
+
 		return GPSUtils.findMax(speeds());
-		
-		// TODO - START
-		
-		
-		// TODO - SLUTT
-		
+
 	}
 
 	public double averageSpeed() {
-		double distance=totalDistance();
-		double time=totalTime();
-		
-		
+		double distance = totalDistance();
+		double time = totalTime();
 
-		return (distance/1000.0)/(time/60.0/60.0);
-		
-		
-		// TODO - START
-		
-		
-		// TODO - SLUTT
-		
+		return (distance / 1000.0) / (time / 60.0 / 60.0);
+
 	}
 
 	/*
-	 * bicycling, <10 mph, leisure, to work or for pleasure 4.0 bicycling,
-	 * general 8.0 bicycling, 10-11.9 mph, leisure, slow, light effort 6.0
-	 * bicycling, 12-13.9 mph, leisure, moderate effort 8.0 bicycling, 14-15.9
-	 * mph, racing or leisure, fast, vigorous effort 10.0 bicycling, 16-19 mph,
-	 * racing/not drafting or >19 mph drafting, very fast, racing general 12.0
-	 * bicycling, >20 mph, racing, not drafting 16.0
+	 * bicycling, <10 mph, leisure, to work or for pleasure 4.0 bicycling, general
+	 * 8.0 bicycling, 10-11.9 mph, leisure, slow, light effort 6.0 bicycling,
+	 * 12-13.9 mph, leisure, moderate effort 8.0 bicycling, 14-15.9 mph, racing or
+	 * leisure, fast, vigorous effort 10.0 bicycling, 16-19 mph, racing/not drafting
+	 * or >19 mph drafting, very fast, racing general 12.0 bicycling, >20 mph,
+	 * racing, not drafting 16.0
 	 */
 
 	// conversion factor m/s to miles per hour
@@ -132,80 +98,67 @@ public class GPSComputer {
 	// beregn kcal gitt weight og tid der kjøres med en gitt hastighet
 	public double kcal(double weight, int secs, double speed) {
 
-		double kcal = weight * secs/speed;
+		double kcal = weight * secs / speed;
 
 		// MET: Metabolic equivalent of task angir (kcal x kg-1 x h-1)
 		double met = 0;
 		double speedmph = speed * MS;
 
-		if (speedmph<10) {
+		if (speedmph < 10) {
 			met = 4.0;
-		} else if (speedmph>=10&&speedmph<12) {
+		} else if (speedmph >= 10 && speedmph < 12) {
 			met = 6.0;
-		} else if (speedmph>=12&&speedmph<14) {
+		} else if (speedmph >= 12 && speedmph < 14) {
 			met = 8.0;
-		} else if (speedmph>=14&&speedmph<16) {
+		} else if (speedmph >= 14 && speedmph < 16) {
 			met = 10.0;
-		}  else if (speedmph>=16&&speedmph<20) {
+		} else if (speedmph >= 16 && speedmph < 20) {
 			met = 12.0;
-		}else if (speedmph>20) {
+		} else if (speedmph > 20) {
 			met = 16.0;
 		}
-		
-		kcal = (met * weight * (secs/3600.0));
-		
+
+		kcal = (met * weight * (secs / 3600.0));
+
 		return kcal;
 	}
 
 	public double totalKcal(double weight) {
-		int fullTid=totalTime();
-		double fullFart=averageSpeed();
+		int fullTid = totalTime();
+		double fullFart = averageSpeed();
 
-		double totalkcal = kcal(weight,fullTid,fullFart);
+		double totalkcal = kcal(weight, fullTid, fullFart);
 		return totalkcal;
 
-		// TODO - START
-		
-
-		// TODO - SLUTT
-		
 	}
-	
+
 	private static double WEIGHT = 80.0;
-	
-	public String displayStatistics() {
-		int time=totalTime()/3600;
-		int minutt= (totalTime()-(3600*time))/60;
-		int sek= (totalTime()-(3600*time)-(60*minutt));
-		String visTime= "" +time;
-		String visMin="" + minutt;
-		String visSek="" + sek;
-		if(time<10) {
-			visTime="0" +time;
-		}
-		if(minutt<10) {
-			visMin="0"+ minutt;
-		}
-		if(sek<10) {
-			visSek="0" + sek;
-		}
-	
-		
 
+	public void displayStatistics() {
+		int time = totalTime() / 3600;
+		int minutt = (totalTime() - (3600 * time)) / 60;
+		int sek = (totalTime() - (3600 * time) - (60 * minutt));
+		String visTime = "" + time;
+		String visMin = "" + minutt;
+		String visSek = "" + sek;
+		if (time < 10) {
+			visTime = "0" + time;
+		}
+		if (minutt < 10) {
+			visMin = "0" + minutt;
+		}
+		if (sek < 10) {
+			visSek = "0" + sek;
+		}
+		
 		System.out.println("==============================================");
-		System.out.println("Total Time 	   :  " + visTime + ":" + visMin + ":" + visSek  );
-		System.out.println("Total Distance :    " + totalDistance()+" km");
+		System.out.println("Total Time 	   :  " + visTime + ":" + visMin + ":" + visSek);
+		System.out.println("Total Distance :    " + totalDistance() + " km");
 		System.out.println("Total Elevation:     " + totalElevation() + " m");
-		System.out.println("Max Speed      :     " + maxSpeed() + " km/t" );
-		System.out.println("Average Speed  :     " + averageSpeed()+ " km/t");
-		System.out.println("Energy         :     "+ totalKcal(WEIGHT) + "kcal");
-		return null;
-
-		// TODO - START
-
-		
-		// TODO - SLUTT
-		
+		System.out.println("Max Speed      :     " + maxSpeed() + " km/t");
+		System.out.println("Average Speed  :     " + averageSpeed() + " km/t");
+		System.out.println("Energy         :     " + totalKcal(WEIGHT) + "kcal");
+		System.out.println("==============================================");
 	}
 
 }
